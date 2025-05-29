d63c5025d4e0abbee04e4d574476ef39f1c4d7b6
edwin <edwin.lopez3@utp.ac.pa>
2025-05-28 21:57:22 -0500

    fea(draginball) Se crea logica a nivel del page 1. Se crea lista de los personajes y poderes. 2. Se crea logica del formulario. 3. Se utilizan eventos para guardar nuevos personajes a traves del formulario.

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,11 +9,46 @@ interface Character {
 
 @Component({
   templateUrl: './dragonball-page.component.html',
+  /*   imports: [NgClass],
+   */
 })
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(1000);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Pikoro', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    };
+  });
+
+  addCharacter(name: string, power: number) {
+    if (!name || !power || power <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: name,
+      power: power,
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+
+    console.log(name, power);
+
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
